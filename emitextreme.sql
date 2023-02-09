75d5481d0ea1f519c05e5ca95629e1a0f463bf8f
Tobias Mattsson <90158047+Klattraren@users.noreply.github.com>
2023-02-09 15:46:03 +0100

    Database without triggers

--- emitextreme.sql
+++ emitextreme.sql
@@ -24,11 +24,6 @@ begin
         foreign key (mail1) References Users(mail),
         foreign key (mail2) References Users(mail));
         
-    Create table Registration(
-		eventId int not null auto_increment,
-        chipId int not null,
-        primary key (eventId,chipId));
-        
 	Create table Track(
 		trackId int primary key not null auto_increment,
         trackName varchar(255) not null,
@@ -57,38 +52,55 @@ begin
         moduleId int not null,
         openForEntry bool not null default 0,
         publicView bool not null default 0,
-        foreign key (trackId) References Track(trackID),
+        foreign key (trackId) References Track(trackId),
         foreign key (hostMail) References Users(mail)
     );
+    
+    Create table Registration(
+		eventId int not null auto_increment,
+        chipId int not null,
+        primary key (eventId,chipId),
+        foreign key (eventId) references Competition(eventId),
+        foreign key (chipId) references Chip(chipId)
+        );
     
     Create table Result(
 		trackTime varchar(255) not null,
         participant1 varchar(255) not null,
         participant2 varchar(255),
-        eventId varchar(255) not null,
+        eventId int not null,
         currentTime DATETIME,
         primary key (participant1,currentTime),
+        foreign key (eventId) references Competition(eventId),
         foreign key (participant1) References Users(mail),
-        foreign key (participant2) References Users(mail),
-        foreign key (eventId) References Competition(eventId)
+        foreign key (participant2) References Users(mail)
     );
 	
-    
-        
-
 end; // DELIMITER ;
 
 DELIMITER //
 create procedure drop_all()
 begin
-	drop table Registration;
+	drop table Result;
+    drop table Registration;
     drop table Competition;
-	drop table Team;
+    drop table Checkpoint;
+    drop table Track;
+    drop table Chip;
 	drop table Users;
 end; // DELIMITER ;
 
+
+
+
+
+drop table Result;
+drop table Competition;
+drop table Checkpoint;
+drop table Track;
+drop table Registration;
+drop table Chip;
 drop table Users;
-drop table Team;
 
 call create_all();
 call drop_all();
@@ -97,7 +109,12 @@ drop procedure drop_all;
 drop procedure create_all;
 
 select * from Users;
-select * from team;
+select * from Competition;
+select * from Chip;
+select * from Registration;
+select * from Track;
+select * from Checkpoint;
+select * from Result;
 
 show tables;
     
