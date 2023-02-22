e1bab20d7766a82523ce973327e220e84005db09
Amin <56772263+Yamin161803@users.noreply.github.com>
2023-02-22 15:44:57 +0100

    test

--- scripts/test.js
+++ scripts/test.js
@@ -1,10 +1,10 @@
-function sendFunction() {
+function createAccount() {
     let xemail = document.getElementById('email').value;
     let xfirst_name = document.getElementById('fname').value;
     let xlast_name = document.getElementById('lname').value;
     let xpassword = document.getElementById('pword').value;
 
-    
+
     fetch('http://193.11.187.227:5000/account', {
         headers: {
             "Content-Type": "application/x-www-form-urlencoded",
@@ -13,7 +13,9 @@ function sendFunction() {
         method: 'PUT',
         body: JSON.stringify({ 'first_name': xfirst_name, 'last_name': xlast_name, 'password': xpassword, 'email': xemail })
     }).then(response => response.json())
-  }
+
+    LogIn.html
+}
 
 
 
