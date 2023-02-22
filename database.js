1000ed94aec0f69bb8ced724fc436292c461fa6c
Miran Ismail <90042468+MiranIsmail@users.noreply.github.com>
2023-02-22 15:38:35 +0100

    SSL/ Node.js to server conection

--- database.js
+++ database.js
@@ -1,10 +1,13 @@
 var mysql = require('mysql');
+const fs = require('fs');
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Grupp5exsport!"
-});
+var con=mysql.createConnection({
+    host:"exsportserver.mysql.database.azure.com", 
+    user:"notadmin", 
+    password:"Grupp5exsport!", 
+    database:"systemteknik", 
+    port:3306, 
+    ssl:{ca:fs.readFileSync("DigiCertGlobalRootCA.crt.pem")}});
 
 con.connect(function(err) {
   if (err) throw err;
