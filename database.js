e0f63708fe50ec4068df1a8f97aab90171d7fdc0
Amin <56772263+Yamin161803@users.noreply.github.com>
2023-02-22 15:14:14 +0100

    connection btween webiste and api

--- database.js
+++ database.js
@@ -1,12 +1,22 @@
 var mysql = require('mysql');
 
+
+
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Grupp5exsport!"
+  password: "Faiz1234",
+  database:"demo"
 });
+
+
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+});
+
+con.query("SELECT * FROM `users`", (err, results) => {
+  if (err) { throw err; }
+  console.log(results);
 });
