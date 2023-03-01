86d615ad3b70f81f1eca15c090f0a3aa20aa0ddb
Tobias Mattsson <tomt21@student.bth.se>
2023-03-01 16:41:54 +0100

    script

--- scripts/js_scripts.js
+++ scripts/js_scripts.js
@@ -186,3 +186,17 @@ function includeHTML2() {
       }
     }
   }
+
+  /*CREATE EVENT */
+let startDate = document.getElementById('startDate')
+let endDate = document.getElementById('endDate')
+
+startDate.addEventListener('change',(e)=>{
+  let startDateVal = e.target.value
+  document.getElementById('startDateSelected').innerText = startDateVal
+})
+
+endDate.addEventListener('change',(e)=>{
+  let endDateVal = e.target.value
+  document.getElementById('endDateSelected').innerText = endDateVal
+}) 
