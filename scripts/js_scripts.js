f458bfcc8ff6a23f7c8ced6da9962115cf87c0c2
Tobias Mattsson <tomt21@student.bth.se>
2023-03-01 15:21:24 +0100

    Stor städ + new navbar och footer

--- scripts/js_scripts.js
+++ scripts/js_scripts.js
@@ -135,29 +135,54 @@ function search_event() {
 
 
 function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /* Loop through a collection of all HTML elements: */
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      /* Make an HTTP request using the attribute value as the file name: */
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /* Remove the attribute, and call this function once more: */
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
-        }
+    var z, i, elmnt, file, xhr;
+    /* Loop through a collection of all HTML elements with the include-html attribute */
+    z = document.querySelectorAll("[include-html]");
+    for (i = 0; i < z.length; i++) {
+      elmnt = z[i];
+      /* Get the file specified in the include-html attribute */
+      file = elmnt.getAttribute("include-html");
+      if (file) {
+        /* Create an XMLHttpRequest object */
+        xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            /* Replace the content of the element with the imported HTML */
+            elmnt.innerHTML = this.responseText;
+          }
+        } 
+        /* Send a request to the server to get the specified file */
+        xhr.open("GET", file, true);
+        xhr.send();
+        /* Exit the function */
+        return;
       }
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /* Exit the function: */
-      return;
     }
   }
-}
+
+  
+function includeHTML2() {
+    var z, i, elmnt, file, xhr;
+    /* Loop through a collection of all HTML elements with the include-html attribute */
+    z = document.querySelectorAll("[include-html2]");
+    for (i = 0; i < z.length; i++) {
+      elmnt = z[i];
+      /* Get the file specified in the include-html attribute */
+      file = elmnt.getAttribute("include-html2");
+      if (file) {
+        /* Create an XMLHttpRequest object */
+        xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            /* Replace the content of the element with the imported HTML */
+            elmnt.innerHTML = this.responseText;
+          }
+        } 
+        /* Send a request to the server to get the specified file */
+        xhr.open("GET", file, true);
+        xhr.send();
+        /* Exit the function */
+        return;
+      }
+    }
+  }
