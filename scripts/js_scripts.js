ab0bc1757c4f609f201f416e43d4a7aef0baedbe
Tobias Mattsson <tomt21@student.bth.se>
2023-03-02 12:05:38 +0100

    card function

--- scripts/js_scripts.js
+++ scripts/js_scripts.js
@@ -47,9 +47,9 @@ function get_user_info() {
     .then((data) => {
       var dataString = String(data).split(',')
       document.getElementById("profileName").innerHTML = dataString[3]
-      document.getElementById("age").innerHTML = dataString[8]
-      document.getElementById("length").innerHTML = dataString[6]
-      document.getElementById("weight").innerHTML = dataString[7]
+      document.getElementById("profile_age").innerHTML = dataString[8]
+      document.getElementById("profile_length").innerHTML = dataString[6]
+      document.getElementById("profile_weight").innerHTML = dataString[7]
     });
 }
 
@@ -144,7 +144,7 @@ function include_HTML() {
   /* Loop through a collection of all HTML elements: */
   z = document.getElementsByTagName("div");
   console.log(z.length)
-  
+
   for (i = 0; i < z.length; i++) {
     element = z[i];
     /*search for elements with a certain atrribute:*/
@@ -170,32 +170,7 @@ function include_HTML() {
     }
   }
 }
-  
-function includeHTML2() {
-    var z, i, elmnt, file, xhr;
-    /* Loop through a collection of all HTML elements with the include-html attribute */
-    z = document.querySelectorAll("[include-html2]");
-    for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-      /* Get the file specified in the include-html attribute */
-      file = elmnt.getAttribute("include-html2");
-      if (file) {
-        /* Create an XMLHttpRequest object */
-        xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-            /* Replace the content of the element with the imported HTML */
-            elmnt.innerHTML = this.responseText;
-          }
-        } 
-        /* Send a request to the server to get the specified file */
-        xhr.open("GET", file, true);
-        xhr.send();
-        /* Exit the function */
-        return;
-      }
-    }
-  }
+
 
   /*CREATE EVENT */
 
@@ -218,3 +193,133 @@ b_day.addEventListener('change',(e)=>{
   document.getElementById('b_day_selected').innerText = b_day_val
 })
 
+/* EVENT PAGE*/
+
+
+function load_events() {
+  var event_text = "New race courses since 2022. Utö is where the whole swimrun movement started and where the late night bet which became ÖTILLÖ was made. It is an island in the southern part of the Stockholm archipelago almost 90 minutes from Stockholm. This is Swimrun heaven!"
+  var event_name = "ÖTILLÖ SWIMRUN UTÖ"
+  var event_date = "17 JUNE 2023"
+  var event_image = "../images/eventimg/otillo.jpg"
+  document.cookie = event_text
+  document.cookie = event_name
+  document.cookie = event_date
+  document.cookie = event_image
+}
+
+
+/*Event card */
+// Create a div element for the card
+function generate_card(){
+  // var value = document.cookie;
+  // var value = document.cookie;
+  // var value = document.cookie;
+  // var value = document.cookie;
+  var event_text = "New race courses since 2022. Utö is where the whole swimrun movement started and where the late night bet which became ÖTILLÖ was made. It is an island in the southern part of the Stockholm archipelago almost 90 minutes from Stockholm. This is Swimrun heaven!"
+  var event_name = "ÖTILLÖ SWIMRUN UTÖ"
+  var event_date = "17 JUNE 2023"
+  var event_image = "../images/eventimg/otillo.jpg"
+
+
+
+
+  var cardDiv = document.createElement('div');
+  cardDiv.classList.add('card', 'event_left_box', 'mb-3', 'drop_shadow');
+  cardDiv.style.listStyle = 'none';
+
+  // Create a div element for the row
+  var rowDiv = document.createElement('div');
+  rowDiv.classList.add('row', 'g-1');
+
+  // Create a div element for the image column
+  var imgColDiv = document.createElement('div');
+  imgColDiv.classList.add('col-md-12');
+
+  // Create an img element for the image
+  var img = document.createElement('img');
+  img.src = event_image;
+  img.classList.add('img-fluid', 'rounded-start');
+  img.alt = '...';
+
+  // Append the image element to the image column
+  imgColDiv.appendChild(img);
+
+  // Create a div element for the card body column
+  var cardBodyColDiv = document.createElement('div');
+  cardBodyColDiv.classList.add('col-md-12');
+
+  // Create a div element for the card body
+  var cardBodyDiv = document.createElement('div');
+  cardBodyDiv.classList.add('card-body', 'card_link');
+
+  // Create a h3 element for the card title
+  var cardTitle = document.createElement('h3');
+  cardTitle.classList.add('card-title');
+  cardTitle.textContent = event_name;
+
+  // Create a h5 element for the card text
+  var cardText = document.createElement('h5');
+  cardText.classList.add('card-text');
+  cardText.textContent = event_date;
+
+  // Create a div element for the text section
+  var textDiv = document.createElement('div');
+
+  // Create a p element for the short text
+  var shortText = document.createElement('p');
+  var shortTextSpan = document.createElement('span');
+  shortTextSpan.id = 'short_text';
+  shortText.appendChild(shortTextSpan);
+
+  // Create a div element for the longer text section
+  var moreTextDiv = document.createElement('div');
+  moreTextDiv.classList.add('more-text');
+  moreTextDiv.style.display = 'none';
+
+  // Create a p element for the long text
+  var longText = document.createElement('p');
+  var longTextSpan = document.createElement('span');
+  longTextSpan.id = 'long_text';
+  longText.appendChild(longTextSpan);
+
+  // Append the short and long text elements to their respective containers
+  textDiv.appendChild(shortText);
+  moreTextDiv.appendChild(longText);
+
+  // Append the text and button elements to the card body
+  cardBodyDiv.appendChild(cardTitle);
+  cardBodyDiv.appendChild(cardText);
+  cardBodyDiv.appendChild(textDiv);
+  cardBodyDiv.appendChild(moreTextDiv);
+
+  // Create a button element for the "Show More" button
+  var showMoreBtn = document.createElement('button');
+  showMoreBtn.classList.add('show-more-btn');
+  showMoreBtn.textContent = 'Show More';
+
+  // Add an event listener to the "Show More" button to toggle the visibility of the longer text section
+  showMoreBtn.addEventListener('click', function() {
+    if (moreTextDiv.style.display === 'none') {
+      moreTextDiv.style.display = 'block';
+      showMoreBtn.textContent = 'Show Less';
+    } else {
+      moreTextDiv.style.display = 'none';
+      showMoreBtn.textContent = 'Show More';
+    }
+  });
+
+  // Append the button element to the card body
+  cardBodyDiv.appendChild(showMoreBtn);
+
+  // Append the image
+  rowDiv.appendChild(imgColDiv);
+
+  // Append the card body column to the row
+  rowDiv.appendChild(cardBodyColDiv);
+
+  // Append the row to the card
+  cardDiv.appendChild(rowDiv);
+
+  // Append the card to the document body
+  document.body.appendChild(cardDiv);
+}
