1123ed933329008684bb87de25ac22cc91753fa7
Amin <56772263+Yamin161803@users.noreply.github.com>
2023-03-22 09:52:56 +0100

    add settings

---
+++ api/include/settings.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Settings.
+ */
+
+define('SALT', 'Water I need water@$£€¤EYsadfuyq3hn5t4i8iw03ho4');
+define('DEBUG_STATUS', 1);
+define('TOKEN_LIFE', 24);
+
+// DB settings
+$host = "localhost";
+$login = "lommdjkz";
+$passwd = "PL18E*xtDf)0d7";
+$db = "lommdjkz_testdata";
