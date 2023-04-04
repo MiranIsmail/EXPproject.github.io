c030452bf3c9af96e84cb04a7dd53959bf19ad64
Amin <56772263+Yamin161803@users.noreply.github.com>
2023-04-04 19:49:43 +0200

    test event

--- scripts/index_load.js
+++ scripts/index_load.js
@@ -139,10 +139,8 @@ async function data_load_index(){
   const response = await fetch("https://rasts.se/api/Event")
   const data = await response.json()
 
-
   data.forEach((i) => {
-    //console.log(i["Name"], i["Date"], i["Description"])
-    generate_card_wide(i["Name"],i["Date"],i["Description"],"../images/eventimg/ronneby.jpg")
+    generate_card_wide(i["event_name"],i["start_date"],i["host_organization"],i["eimage"])
   })
 }
 
