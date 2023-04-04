febe3b44062d9636b0a2b72d327697b7b16fe8d7
Amin <56772263+Yamin161803@users.noreply.github.com>
2023-04-04 19:09:49 +0200

    log out button show when loged in

--- scripts/js_scripts.js
+++ scripts/js_scripts.js
@@ -328,11 +328,13 @@ async function update_navbar() {
   if (get_cookie("auth_token")) {
 
     const response = await fetch(BASE_ULR + "Token", {
-      method: 'PATCH',
+      method: 'GET',
       headers: { 'Content-Type': 'application/json', 'Authorization': get_cookie('auth_token') }
     })
     status_code = await response.status
+    console.log(status_code)
   }
+  console.log(status_code)
 
   if (status_code == 200) {
     document.getElementById("navbar-log-out").classList.remove("d-none")
