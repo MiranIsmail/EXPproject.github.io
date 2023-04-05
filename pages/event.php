8a8eb10875f9cb3ac9ebc3db0bc6ef47b5979c4d
Tobias Mattsson <tomt21@student.bth.se>
2023-04-05 20:28:36 +0200

    new page

--- pages/event.php
+++ pages/event.php
@@ -18,44 +18,17 @@
 
 <body>
   <?php include '../assets/navbar.php'; ?>
-  <div>
     <img class="w-100 op30" src="../images/indeximage_thinner.png" id="image_run">
-    <div style="padding-bottom: 4rem;" id="searchFade">
-    </div>
+    <div style="padding-bottom: 4rem;" id="searchFade"></div>
 
+
+
+
+    
     <div class="events section w-100" style="padding-bottom: 4rem;">
-      <div class="row w-100">
-        <div class="col-md-6" id="col_devider">
-          <div class="nice_text">
-            <h1 style="font-family: var(--bs-body-font-family); margin-left: 35%;">Create Event</h1>
-          </div>
-          <button type="button" onclick="location.href='eventcreate.php'" class="btn btn-primary" style="margin-left: 45%;">Create Event</button>
-        </div>
-
-        <div class="col-md-6">
-          <h1 class="current_events">
-            <div class="nice_text">
-              <h1 style="font-family: var(--bs-body-font-family); margin-left: 25%;">Current Event</h1>
-            </div>
-          </h1>
-          <!--Left-->
-          <div class="drop_shadow">
-            <div class="searchBar">
-              <input id="searchQueryInput" type="text" name="searchQueryInput" placeholder="Search" value="" onkeyup="search_event()" />
-              <button id="searchQuerySubmit" type="submit" name="searchQuerySubmit">
-                <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                  <path fill="#666666" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
-                </svg>
-              </button>
-            </div>
-          </div>
-          <div id="event_cards_dynamic"></div>
-        </div>
-
-      </div>
-      </table>
-      </a>
+      <div id="event_cards_dynamic"></div>
     </div>
+
     <?php include '../assets/footer.php'; ?>
 </body>
 
