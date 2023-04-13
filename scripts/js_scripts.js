5824a910a8c19b3fb80c26589703c883a5ac657e
Tobias Mattsson <tomt21@student.bth.se>
2023-04-13 10:29:00 +0200

    test

--- scripts/js_scripts.js
+++ scripts/js_scripts.js
@@ -383,10 +383,11 @@ async function create_event() {
   parameters["enddate"] = document.getElementById('send_end_date').value
   parameters["eimage"] = document.getElementById('send_image').value
   parameters["description"] = document.getElementById('send_description').value
-  // parameters["open_for_entry"] = document.getElementById('send_open').value
-  // parameters["public_view"] = document.getElementById('send_public').value
-  parameters["open_for_entry"] = document.getElementById('send_open').checked
-  parameters["public_view"] = document.getElementById('send_public').checked
+  parameters["open_for_entry"] = document.getElementById('send_open').value
+  parameters["public_view"] = document.getElementById('send_public').value
+
+  // parameters["open_for_entry"] = document.getElementById('send_open').checked
+  // parameters["public_view"] = document.getElementById('send_public').checked
 
   console.log(parameters["open_for_entry"])
   console.log(parameters["public_view"])
