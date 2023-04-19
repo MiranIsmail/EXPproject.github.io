c85dd8f2a9e58938a2fadcd809be565cf6d10839
Amin <56772263+Yamin161803@users.noreply.github.com>
2023-04-19 14:41:05 +0200

    test email

--- assets/organization_request.php
+++ assets/organization_request.php
@@ -2,15 +2,15 @@
 if (empty($_GET)) {
     header('Location: ' . "https://www.rasts.se/");
 }
-$msg = "Organisation name: " . $_GET["org_name"] . "\n Adress: " . $_GET["address"] . "\n email: " . $_GET["org_email"] . "\n Organisation number: " . $_GET["org_number"] . "\n Contact email: " . $_GET["contact_email"] . "\n Contact number" . $_GET["number"];
+$msg_rast = "Organization name: " . $_GET["org_name"] . "\n Adress: " . $_GET["address"] . "\n email: " . $_GET["org_email"] . "\n Organization number: " . $_GET["org_number"] . "\n Contact email: " . $_GET["contact_email"] . "\n Contact number" . $_GET["number"];
 
-"First line of text\nSecond line of text";
+$msg = "Thank you for your request to register your organization. We will get back to you as soon as possible with more information on how to set up your organizational account. We will be in touch!";
 
 // use wordwrap() if lines are longer than 70 characters
-$msg = wordwrap($msg, 70);
+$msg_rast = wordwrap($msg_rast, 70);
 
 // send email
 // org nummber
-
+mail("info@rasts.se", "Organization request", $msg_rast);
 mail($_GET["contact_email"], "Organization request", $msg);
 header('Location: ' . "https://www.rasts.se/");
