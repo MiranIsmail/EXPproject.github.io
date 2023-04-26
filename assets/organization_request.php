ba93e70701939fc89326aa14fe2e910069ad8160
Amin <56772263+Yamin161803@users.noreply.github.com>
2023-04-26 16:00:09 +0200

    test

--- assets/organization_request.php
+++ assets/organization_request.php
@@ -4,7 +4,7 @@ if (empty($_GET)) {
   header('Location: ' . "https://www.rasts.se/");
 }
 $user_data = get_user_info();
-
+var_dump($user_data);
 $msg_rast = "Organization name: " . $_GET["org_name"] . "\n Adress: " . $_GET["address"] . "\n email: " . $_GET["org_email"] . "\n Organization number: " . $_GET["org_number"] . "\n Contact email: " . $_GET["contact_email"] . "\n Contact number" . $_GET["number"] . "\n $_GET[org_name], $_GET[address], $_GET[org_email], $_GET[org_number], $user_data->username";
 
 $msg = "Thank you for your request to register your organization. We will get back to you as soon as possible with more information on how to set up your organizational account. We will be in touch!";
