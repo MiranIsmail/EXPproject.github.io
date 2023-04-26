9644429301d1b09f64d18f4297512e3c74fa0b7c
Amin <56772263+Yamin161803@users.noreply.github.com>
2023-04-26 15:55:40 +0200

    organization creation

--- pages/profile.php
+++ pages/profile.php
@@ -31,60 +31,60 @@
                         <h3 class="text-shift">Address: <span id="profile_weight"></span><?= $organization_data->address ?> </h3>
                         <h3 class="text-shift">Description: <span id="profile_weight"></span><?= $organization_data->description ?> </h3>
 
-                    <?php } else{?>
+                    <?php } ?>
                     <form action="../assets/organization_request.php" class="orgform" method="GET">
                         <div class=" form-group form_group_style mx-auto">
-                        <button class="button-modular" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Want to register your organisation?</button>
+                            <button class="button-modular" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Want to register your organisation?</button>
 
-                        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-                            <div class="offcanvas-header">
-                                <h5 id="offcanvasRightLabel">Register your organisation</h5>
-                                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                            </div>
-                            <div class="offcanvas-body canvas_container">
-                                To make sure you are an organisation, we would like you till fill this form:
-                                <div class="form-group form_group_style mx-auto needs-validation" novalidate>
-                                    <div class="form-group form_group_style mx-auto"></div>
-                                    <p>Organisation name</p>
+                            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+                                <div class="offcanvas-header">
+                                    <h5 id="offcanvasRightLabel">Register your organisation</h5>
+                                    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                </div>
+                                <div class="offcanvas-body canvas_container">
+                                    To make sure you are an organisation, we would like you till fill this form:
+                                    <div class="form-group form_group_style mx-auto needs-validation" novalidate>
+                                        <div class="form-group form_group_style mx-auto"></div>
+                                        <p>Organisation name</p>
 
-                                    <input type="text" name="org_name" class="form-control input_field_style" placeholder="Name" id="org_name" required>
+                                        <input type="text" name="org_name" class="form-control input_field_style" placeholder="Name" id="org_name" required>
 
-                                </div>
-                                <div class="form-group form_group_style mx-auto">
-                                    <p>Address</p>
-                                    <input type="text" name="address" class="form-control input_field_style" placeholder="Region of domain" id="org_country" required>
+                                    </div>
+                                    <div class="form-group form_group_style mx-auto">
+                                        <p>Address</p>
+                                        <input type="text" name="address" class="form-control input_field_style" placeholder="Region of domain" id="org_country" required>
 
+                                    </div>
+                                    <div class="form-group form_group_style mx-auto">
+                                        <p>Email Address for organisation </p>
+                                        <input type="email" name="org_email" class="form-control input_field_style" placeholder="expproject@gmail.com" id="org_email" required>
+                                    </div>
+                                    <div class="form-group form_group_style mx-auto">
+                                        <p>Organisation number </p>
+                                        <input type="text" name="org_number" class="form-control input_field_style" placeholder="111111-0000" id="org_email" required>
+                                    </div>
+                                    <h2>Contact information</h2>
+                                    <div class="form-group form_group_style mx-auto">
+                                        <p>Private Email Address </p>
+                                        <input type="text" name="contact_email" class="form-control input_field_style" placeholder="expproject@gmail.com" id="user_email" required>
+                                    </div>
+                                    <div class="form-group form_group_style mx-auto">
+                                        <p>Phone Number </p>
+                                        <input type="tel" name=" number" class="form-control input_field_style" placeholder="+46" id="user_email" required>
+                                    </div>
+                                    <div id="success-alert" class="alert alert-success alert-dismissible fade show d-none" role="alert">
+                                        Form submitted successfully!
+                                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                    </div>
+                                    <div class="form-group form_group_style mx-auto">
+                                        <button class="btn btn-primary" button id="submit_org_form" type="submit" onclick="fill_org_form()" value="Submit">Submit form</button>
+                                    </div>
+                                    After submission we will as soon as possible get back to you to give you more information on how to set up your organisational account. We will be in touch!
                                 </div>
-                                <div class="form-group form_group_style mx-auto">
-                                    <p>Email Address for organisation </p>
-                                    <input type="email" name="org_email" class="form-control input_field_style" placeholder="expproject@gmail.com" id="org_email" required>
-                                </div>
-                                <div class="form-group form_group_style mx-auto">
-                                    <p>Organisation number </p>
-                                    <input type="text" name="org_number" class="form-control input_field_style" placeholder="111111-0000" id="org_email" required>
-                                </div>
-                                <h2>Contact information</h2>
-                                <div class="form-group form_group_style mx-auto">
-                                    <p>Private Email Address </p>
-                                    <input type="text" name="contact_email" class="form-control input_field_style" placeholder="expproject@gmail.com" id="user_email" required>
-                                </div>
-                                <div class="form-group form_group_style mx-auto">
-                                    <p>Phone Number </p>
-                                    <input type="tel" name=" number" class="form-control input_field_style" placeholder="+46" id="user_email" required>
-                                </div>
-                                <div id="success-alert" class="alert alert-success alert-dismissible fade show d-none" role="alert">
-                                    Form submitted successfully!
-                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                                </div>
-                                <div class="form-group form_group_style mx-auto">
-                                    <button class="btn btn-primary" button id="submit_org_form" type="submit" onclick="fill_org_form()" value="Submit">Submit form</button>
-                                </div>
-                                After submission we will as soon as possible get back to you to give you more information on how to set up your organisational account. We will be in touch!
                             </div>
                         </div>
-                    </div>
                     </form>
-        <?php } ?>
+
                 </div>
             </div>
         </div>
