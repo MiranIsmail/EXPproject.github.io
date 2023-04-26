8de2fc48a211d8d5d27f44bc85686f88b873439d
Tobias Mattsson <tomt21@student.bth.se>
2023-04-26 13:24:56 +0200

    added username to profile page, added search on profile page

--- pages/profile.php
+++ pages/profile.php
@@ -14,6 +14,7 @@
                     <h1 class="text-shift">
                         <span id="profileName">Profile name</span>
                     </h1>
+                    <h3 class="text-shift">Username: <span id="profile_username"></span></h3>
                     <h3 class="text-shift">Age: <span id="profile_age"></span> years</h3>
                     <h3 class="text-shift">Length: <span id="profile_length"></span> cm</h3>
                     <h3 class="text-shift">Weight: <span id="profile_weight"></span> kg</h3>
