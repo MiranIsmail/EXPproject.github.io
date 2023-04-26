e6e00ecb3cad04c6b19942d12c618f8d5266f10e
fimbo4 <99178906+fimbo4@users.noreply.github.com>
2023-04-26 14:01:37 +0200

    Update timetable.php

--- pages/timetable.php
+++ pages/timetable.php
@@ -4,21 +4,8 @@
   <?php include '../assets/navbar.php'; ?>
 
   <script>
-
-    // class checkpoint { //Not a necessary class right now but might be useful for manipulating data later on
-    //   constructor(station_name, time_stamp, diff_time_stamp, diff_sec,
-    //   checkpoint_result_id, result_id){
-    //     this.station_name = station_name
-    //     this.time_stamp = time_stamp 
-    //     this.diff_time_stamp = diff_time_stamp
-    //     this.diff_sec = diff_sec
-    //     this.checkpoint_result_id = checkpoint_result_id
-    //     this.result_id = result_id
-    //     }
-    //   };
-    
     async function GetChecks(result_id, event_id, token, track_name){
-      //don't judge anything you read in this file, thank you
+      //calls the api and fills the html table with data
 
       checkpoint_time = await fetch("https://rasts.se/api/Results/" + result_id.toString(), {method:'GET',
       headers: {'Accept': 'Application/json'}})
@@ -30,11 +17,7 @@
       data = await checkpoint_time.json();
       data1 = await result_data.json()
       data2 = await event_data.json()
-      //console.log(data2)
       data3 = await checkpoint_data.json()
-      //for(let i = 0; i < data2.length; i++){
-        //if(data2[i].event_ids == event_id){
-        //document.getElementById('event_title').innerHTML = "Event: " + data2.event_name
         document.getElementById('track_title').innerHTML = "Track: " + track_name
         document.getElementById('date').innerHTML = "Date: " + data2.results[0].DATE
       
@@ -68,18 +51,16 @@
       <tbody>
         <script>
           function FillTable(data, data1, data2, data3){
-            
-            //  
-            // 
+
           for (let i = 0; i < data.length; i++) {
             let row = timetable.insertRow(i + 1)
-            let cell1 = row.insertCell(0)
-            let cell2 = row.insertCell(1)
-            let cell3 = row.insertCell(2)
-            let cell4 = row.insertCell(3)
-            let cell5 = row.insertCell(4)
-            let cell6 = row.insertCell(5)
-            let cell7 = row.insertCell(6)
+            let cell1 = row.insertCell(0) //station name
+            let cell2 = row.insertCell(1) //time in seconds
+            let cell3 = row.insertCell(2) //start time
+            let cell4 = row.insertCell(3) //end time
+            let cell5 = row.insertCell(4) //terrain
+            let cell6 = row.insertCell(5) //distance
+            let cell7 = row.insertCell(6) //average time
             if (i == 0) {
               cell1.innerHTML = data[i].station_name + " (Start)"
               if(data[i+1]){
@@ -95,9 +76,7 @@
               cell2.innerHTML = TimeDiff(data[i].time_stamp, data[i+1].time_stamp)
               cell4.innerHTML = data[i+1].time_stamp
             }
-            // cell2.innerHTML = data[i].time_stamp
             cell3.innerHTML = data[i].time_stamp
-            // cell4.innerHTML = data[i].diff_sec
             if(data1[i]){
             cell5.innerHTML = data1[i].terrain
             }
@@ -109,16 +88,16 @@
             }
           }
         }
-        function TimeDiff(time1, time2){
+        function TimeDiff(time1, time2){//difference between two times in seconds
             time1 = ConvertTime(time1)
             time2 = ConvertTime(time2)
             return time2 - time1
         }
-        function AverageVel(distance, time_diff){
+        function AverageVel(distance, time_diff){//average velocity
           return (distance/time_diff)
         }
         function ConvertTime(time_string){
-          h1 = time_string[0]
+          h1 = time_string[0]//super advanced constant runtime hours/minute/seconds to seconds convertation algorithm
           h2 = time_string[1]
           m1 = time_string[3]
           m2 = time_string[4]
@@ -132,14 +111,10 @@
           hours = parseInt(hours) * 60 * 60
           return hours + minutes + seconds
         }
-          //console.log(ConvertTime("23:35:52.3275"))
-          console.log(ConvertTime("00:00:30.0000"))
-          GetChecks(95, 12, "c5bd7dec836ce331ce40a875a5406d3fa1f5942fd9e38d21a5ec2dbee7163d43", "Adams femte bana")
         </script>
       </tbody>
     </table>
   </div>
   <?php include '../assets/footer.php'; ?>
-  <!-- <script type="text/javascript" src="../scripts/timetable.js"></script> -->
 
 </body>
