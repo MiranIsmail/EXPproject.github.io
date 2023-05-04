dcfcabd52c20a70f48eec4036d07a4d9bb9a38a9
Tobias Mattsson <tomt21@student.bth.se>
2023-05-04 14:36:37 +0200

    push

--- pages/create_password.php
+++ pages/create_password.php
@@ -1,22 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <title>Signup - Rasts</title>
-    <meta charset="utf-8">
-    <link rel="icon" type="image/x-icon" href="../images/logo_color.png">
-    <!--Tre librarys dont remove, Bootstrap 5-->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="../styles/stylesheet.css">
-    <link rel="stylesheet" href="../styles/login_and_signup.css">
-    <script type="text/javascript" src="../scripts/js_scripts.js"></script>
-</head>
-<!--header-->
+<?php
+include '../assets/head.php';
+?>
 
 <body>
-    <?php include '../assets/navbar.php'; ?>
+  <?php include '../assets/navbar.php'; ?>
     <!--The swimrun image-->
     <div class="image_div">
         <img class="w-100 op30" src="../images/indeximage_thinner.png" id="image_run" alt="Running figures">
