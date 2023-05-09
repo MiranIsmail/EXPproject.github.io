cdb198ad68459aa065d01b2a6468b8f9fc0df13a
Omardll001 <omar.dalal.2001@gmail.com>
2023-05-09 14:39:56 +0200

    confitm_email_sent_message

--- pages/confirmation_account.php
+++ pages/confirmation_account.php
@@ -6,6 +6,7 @@ include '../assets/head.php';
   <?php include '../assets/navbar.php'; ?>
 <div class="container">
   <h1 class="mx-auto" style="text-align: center; padding-top: 20rem;">Thank You for Creating an Account!</h1>
+  <p class="mx-auto" style="text-align: center;">Please check your email inbox to confirm your email.</p>
   <p class="mx-auto" style="text-align: center;">You will be redirected in <span id="countdown">3</span> seconds.</p>
 </div>
 
