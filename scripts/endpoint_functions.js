41ebecefcf1477bbd34dd5f9afc17eccd9e11beb
AdamMutzell <admt21@student.bth.se>
2023-05-11 14:13:58 +0200

    delete token

--- scripts/endpoint_functions.js
+++ scripts/endpoint_functions.js
@@ -1,5 +1,3 @@
-
-
 /**
  * endpoint for creating an account
  * @param {string} email email of the user
@@ -10,20 +8,26 @@
  * @returns a fetch response with data about the user in json format. The data includes first_name, last_name, email, username, birthdata, profile image, chip_id, height and weight.
  */
 
-async function create_account_endpoint(email, first_name, last_name, password, username) {
-    return await fetch(BASE_ULR + "Account", {
-        method: 'POST',
-        body: JSON.stringify({
-            email: email,
-            first_name: first_name,
-            last_name: last_name,
-            password: password,
-            username: username
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+async function create_account_endpoint(
+  email,
+  first_name,
+  last_name,
+  password,
+  username
+) {
+  return await fetch(BASE_ULR + "Account", {
+    method: "POST",
+    body: JSON.stringify({
+      email: email,
+      first_name: first_name,
+      last_name: last_name,
+      password: password,
+      username: username,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 /**
@@ -32,10 +36,9 @@ async function create_account_endpoint(email, first_name, last_name, password, u
  * @returns a fetch response with data about the user in json format. The data includes the public information about the user. *
  * */
 async function get_account_details_endpoint(username) {
-    return await fetch(BASE_ULR + "Account/" + username, {
-        method: "GET",
-    });
-
+  return await fetch(BASE_ULR + "Account/" + username, {
+    method: "GET",
+  });
 }
 
 /**
@@ -44,16 +47,14 @@ async function get_account_details_endpoint(username) {
  * @returns a fetch response with empty data.
  */
 async function edit_user_details_endpoint(parameters) {
-    return await fetch(BASE_ULR + "Account", {
-        method: "PATCH",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: get_cookie("auth_token"),
-        },
-        body: JSON.stringify(parameters),
-    });
-
-
+  return await fetch(BASE_ULR + "Account", {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: get_cookie("auth_token"),
+    },
+    body: JSON.stringify(parameters),
+  });
 }
 
 /**
@@ -62,11 +63,10 @@ async function edit_user_details_endpoint(parameters) {
  * @returns a fetch response with data about the user in json format. The data includes first_name, last_name, email, username, birthdata, profile image, chip_id, height and weight.
  */
 async function get_user_details_endpoint(token) {
-    return await fetch(BASE_ULR + "Account", {
-        method: "GET",
-        headers: { Authorization: token },
-    });
-
+  return await fetch(BASE_ULR + "Account", {
+    method: "GET",
+    headers: { Authorization: token },
+  });
 }
 
 /**
@@ -77,12 +77,11 @@ async function get_user_details_endpoint(token) {
  * @returns a fetch response with data about the user in json format. The data includes the token of the user. The token is active for 24 hours.
  */
 async function get_token_endpoint(email, password, table_name = "Users") {
-    return await fetch(BASE_ULR + "Token/" + table_name, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email: email, password: password }),
-    });
-
+  return await fetch(BASE_ULR + "Token/" + table_name, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email: email, password: password }),
+  });
 }
 /**
  * changes the token without returning any data.
@@ -90,11 +89,21 @@ async function get_token_endpoint(email, password, table_name = "Users") {
  * @param {string} table_name the name of the table that the token is for
  * @returns a fetch response with empty data.
  */
-async function delete_token_endpoint(token, table_name = "Users") {
-    return await fetch(BASE_ULR + "Token/" + table_name, {
-        method: "DELETE",
-        headers: { Authorization: token },
-    });
+function delete_token_endpoint(token, table_name = "Users") {
+  const xhr = new XMLHttpRequest();
+  const url = BASE_ULR + "Token/" + table_name;
+  xhr.open("DELETE", url, true);
+  xhr.setRequestHeader("Authorization", token);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        console.log("Token successfully deleted");
+      } else {
+        console.log("Error deleting token");
+      }
+    }
+  };
+  xhr.send();
 }
 
 /**
@@ -103,110 +112,98 @@ async function delete_token_endpoint(token, table_name = "Users") {
  * @returns a fetch response with data about the users results in json format. The data includes all the .
  */
 async function get_user_result_endpoint(token) {
-    return await fetch(BASE_ULR + "Results/?token=" + token);
-
+  return await fetch(BASE_ULR + "Results/?token=" + token);
 }
 
 async function get_result_endpoint(result_id) {
-    return await fetch(BASE_ULR + "Results/" + result_id);
-
+  return await fetch(BASE_ULR + "Results/" + result_id);
 }
 
 async function get_event_results_endpoint(event_id) {
-    return await fetch(BASE_ULR + "Results/?event_id=" + event_id);
-
+  return await fetch(BASE_ULR + "Results/?event_id=" + event_id);
 }
 
 async function get_account_result_endpoint(username) {
-    return await fetch(BASE_ULR + "Results/?username=" + username);
-
+  return await fetch(BASE_ULR + "Results/?username=" + username);
 }
 
 async function get_event_endpoint(event_id) {
-    return await fetch(BASE_ULR + "Event/" + event_id);
-
+  return await fetch(BASE_ULR + "Event/" + event_id);
 }
 async function get_all_event_endpoint() {
-    return await fetch(BASE_ULR + "Event");
-
+  return await fetch(BASE_ULR + "Event");
 }
 async function get_top_event_endpoint() {
-    return await fetch(BASE_ULR + "Event?setting=topten");
-
+  return await fetch(BASE_ULR + "Event?setting=topten");
 }
 
 async function get_track_checkpoints_endpoint(track_name) {
-    return await fetch(BASE_ULR + "Checkpoint?track_name=" + track_name);
-
+  return await fetch(BASE_ULR + "Checkpoint?track_name=" + track_name);
 }
 
-
-
 function create_track_endpoint(track_name, start_station, end_station) {
-    xhr = new XMLHttpRequest();
-    xhr.open("POST", BASE_ULR + "Track", false); // false makes the request synchronous
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.send(
-        JSON.stringify({
-            track_name: track_name,
-            start_station: start_station,
-            end_station: end_station,
-        })
-    );
+  xhr = new XMLHttpRequest();
+  xhr.open("POST", BASE_ULR + "Track", false); // false makes the request synchronous
+  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhr.send(
+    JSON.stringify({
+      track_name: track_name,
+      start_station: start_station,
+      end_station: end_station,
+    })
+  );
 }
 
 async function create_checkpoint_endpoint(
-    trackname,
-    startid,
-    endid,
-    dist,
-    terrain,
-    longitude,
-    latitude,
-    number
+  trackname,
+  startid,
+  endid,
+  dist,
+  terrain,
+  longitude,
+  latitude,
+  number
 ) {
-    await fetch(BASE_ULR + "Checkpoint", {
-        method: "POST",
-        body: JSON.stringify({
-            track_name: trackname,
-            station_id: startid,
-            next_id: endid,
-            next_distance: dist,
-            terrain: terrain,
-            longitude: longitude,
-            latitude: latitude,
-            checkpoint_number: number,
-        }),
-        headers: { "Content-Type": "application/json; charset=UTF-8" },
-    });
+  await fetch(BASE_ULR + "Checkpoint", {
+    method: "POST",
+    body: JSON.stringify({
+      track_name: trackname,
+      station_id: startid,
+      next_id: endid,
+      next_distance: dist,
+      terrain: terrain,
+      longitude: longitude,
+      latitude: latitude,
+      checkpoint_number: number,
+    }),
+    headers: { "Content-Type": "application/json; charset=UTF-8" },
+  });
 }
 
-async function create_event_endpoint(parameters
-) {
-
-    return await fetch(BASE_ULR + "Event", {
-        method: "POST",
-        body: JSON.stringify(parameters),
-        headers: { "Content-Type": "application/json; charset=UTF-8" },
-    });
+async function create_event_endpoint(parameters) {
+  return await fetch(BASE_ULR + "Event", {
+    method: "POST",
+    body: JSON.stringify(parameters),
+    headers: { "Content-Type": "application/json; charset=UTF-8" },
+  });
 }
 
 async function get_all_tracks_endpoint() {
-    const response = await fetch(BASE_ULR + "Track");
-    return await response
+  const response = await fetch(BASE_ULR + "Track");
+  return await response;
 }
 
 async function generate_event_results_endpoint(event_id) {
-    const response = await fetch(BASE_ULR + "Results?event_id=" + event_id);
-    return await response
+  const response = await fetch(BASE_ULR + "Results?event_id=" + event_id);
+  return await response;
 }
 
 async function register_event_endpoint(parameters) {
-    const response = await fetch(BASE_ULR + "Registration", {
-        method: "POST",
-        body: JSON.stringify(parameters),
-    });
-    return await response
+  const response = await fetch(BASE_ULR + "Registration", {
+    method: "POST",
+    body: JSON.stringify(parameters),
+  });
+  return await response;
 }
 
 /**
@@ -215,8 +212,7 @@ async function register_event_endpoint(parameters) {
  * @returns
  */
 async function get_user_upcoming_endpoint(username) {
-    return await fetch(BASE_ULR + `Registration/?username=${username}`, {
-        method: "GET",
-    });
-
+  return await fetch(BASE_ULR + `Registration/?username=${username}`, {
+    method: "GET",
+  });
 }
