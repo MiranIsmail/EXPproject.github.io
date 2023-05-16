822707ffdaa8dc85828e35bee6d39395a0e979e7
fimbo4 <99178906+fimbo4@users.noreply.github.com>
2023-05-16 14:18:54 +0200

    Update js_scripts.js

--- scripts/js_scripts.js
+++ scripts/js_scripts.js
@@ -363,7 +363,6 @@ async function update_user_password(event) {
 
 async function GetChecks(result_id, event_id, speed_unit, distance_unit) {
   //calls the api and fills the html table with data
-  console.log(speed_unit)
   check_time = await get_result_endpoint(result_id)
   check_time = await check_time.json()
 
@@ -402,8 +401,6 @@ function FillTable(check_time, check_terrain, speed_unit, distance_unit) {
     }
   });
   //#############################################################################
-  console.log(speed_unit)
-  console.log(distance_unit)
   let total_time = 0
   let total_dist = 0
   for (let i = 0; i < check_terrain.length; i++) {
@@ -453,6 +450,18 @@ function FillTable(check_time, check_terrain, speed_unit, distance_unit) {
                         .diff_sec
                     )
                   )).toFixed(1) + " knots";}
+                  else if(speed_unit=='kpm'){
+                    cell5.innerHTML =
+                     convert_kpm(AverageVel(
+                        parseInt(
+                          check_terrain[dict[check_terrain[i].station_id][0]].next_distance
+                        ),
+                        parseInt(
+                          check_time.result[dict[check_terrain[i + 1].station_id][0]]
+                            .diff_sec
+                        )
+                      )).toFixed(1) + " kpm";
+                  }
               else{
                 cell5.innerHTML =
                  (AverageVel(
@@ -591,6 +600,18 @@ function FillTable(check_time, check_terrain, speed_unit, distance_unit) {
                         .diff_sec
                     )
                   )).toFixed(1) + " knots";}
+              else if(speed_unit=='kpm'){
+                cell5.innerHTML =
+                 convert_kpm(AverageVel(
+                    parseInt(
+                      check_terrain[dict[check_terrain[i].station_id][0]].next_distance
+                    ),
+                    parseInt(
+                      check_time.result[dict[check_terrain[i + 1].station_id][0]]
+                        .diff_sec
+                    )
+                  )).toFixed(1) + " kpm";
+              }
               else{
                 cell5.innerHTML =
                  (AverageVel(
@@ -607,7 +628,7 @@ function FillTable(check_time, check_terrain, speed_unit, distance_unit) {
     }
     if(i == check_terrain.length - 1){
       cell1.innerHTML = "Total:"
-      cell2.innerHTML = total_time.toString() + "(s)"
+      cell2.innerHTML = total_time.toString() + "s"
       if(distance_unit == 'km')
       {
         cell4.innerHTML = convert_kilo(total_dist) + " km"
@@ -621,13 +642,12 @@ function FillTable(check_time, check_terrain, speed_unit, distance_unit) {
         cell4.innerHTML = convert_naut(total_dist) + " nm"
       }
       else{
-        cell4.innerHTML = total_dist + " m";
+        cell4.innerHTML = total_dist + "m";
       }
       
 
     }
   }
-  console.log(convert_knots(10))
 }
 function TimeDiff(time1, time2) {
   //difference between two times in seconds
@@ -659,13 +679,11 @@ function pretty_print_time(ts) {
   return ts[0] + ts[1] + "h " + ts[3] + ts[4] + "m " + ts[6] + ts[7] + "s";
 }
 
-async function timetable_link_func() {
+async function timetable_link_func(speed_unit, distance_unit) {
   const urlParams = new URLSearchParams(window.location.search);
   event_id = urlParams.get("event_id");
   result_id = urlParams.get("result_id");
-  speed_unit = urlParams.get("su");
-  distance_unit = urlParams.get("du");
-  GetChecks(result_id, event_id, speed_unit, distance_unit);
+  GetChecks(speed_unit, distance_unit, speed_unit, distance_unit);
 }
 
 function convert_kmh(ms){
@@ -680,6 +698,10 @@ function convert_knots(ms){
   return (ms * 3600) / 1852
 }
 
+function convert_kpm(ms){
+  return (ms / 16.667)
+}
+
 function convert_kilo(m){
   return (m / 1000).toFixed(2)
 }
@@ -690,4 +712,8 @@ function convert_mile(m){
 
 function convert_naut(m){
   return (m / 1852).toFixed(2)
+}
+
+function convert_feet(m){
+  return (m * 3.28084).toFixed(2)
 }
