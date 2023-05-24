16616a80a111dba2ed44e0eb00ae222c91eab1b3
miran <miim21@student.bth.se>
2023-05-24 16:10:20 +0200

    brr

---
+++ pages/Resorces.php
@@ -0,0 +1,18 @@
+<?php
+include '../assets/head.php';
+?>
+
+<body>
+    <?php include '../assets/navbar.php'; ?>
+    <!--The swimrun image-->
+    <div class="image_div">
+        <img class="w-100 op30" src="../images/indeximage_thinner.png" id="image_run">
+    </div>
+    <div>
+    <a href="..\rasts.zip" class="btn btn-primary" download>Download</a>
+    <div\>
+    <?php include '../assets/footer.php'; ?>
+
+</body>
+
+</html>
