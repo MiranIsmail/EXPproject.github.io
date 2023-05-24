29f6fe1bdfde2cb6a31bdef90f30ad249a331921
miran <miim21@student.bth.se>
2023-05-24 16:41:18 +0200

    brrr

--- pages/resource.php
+++ pages/resource.php
@@ -5,12 +5,32 @@ include '../assets/head.php';
 <body>
     <?php include '../assets/navbar.php'; ?>
     <!--The swimrun image-->
+
     <div class="image_div">
         <img class="w-100 op30" src="../images/indeximage_thinner.png" id="image_run">
     </div>
-    <div>
-    <a href="..\rasts.zip" class="btn btn-primary" download>Download</a>
-    <div\>
+    <div class="events">
+            <div>
+                <h1 class="text-center">Resources</h1>
+                <p class="text-center">Here you can find resources for Organizations</p>
+                <div class="container">
+                    <table class="table table-secondary">
+                        <thead>
+                            <tr>
+                                <th>Resource</th>
+                                <th>Download Link</th>
+                            </tr>
+                        </thead>
+    <tbody>
+      <tr>
+        <td>Rasts application for organizers</td>
+        <td><a href="..\rasts.zip" class="btn btn-primary" download>Download</a></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+</div>
+<div\>
     <?php include '../assets/footer.php'; ?>
 
 </body>
